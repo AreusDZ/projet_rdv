563fe60d8ead443b9026503adcb1673d85c11d4a
AreusDZ <samir-mekhloufi@hotmail.fr>
2021-01-12 11:56:47 +0100

    fin Api Rest pour rendez vous

--- projet-doctolib/src/Controller/RendezVousRestController.php
+++ projet-doctolib/src/Controller/RendezVousRestController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 
 use App\Entity\RendezVous;
-use App\Mapper\RendezVousMapper;
+use App\Entity\RendezVousDTO;
 use FOS\RestBundle\View\View;
+use App\Mapper\RendezVousMapper;
 use App\Service\RendezVousService;
-use App\Service\RendezVousServiceException;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Service\RendezVousServiceException;
 use FOS\RestBundle\Controller\Annotations\Get;
 use Symfony\Component\HttpFoundation\Response;
+use FOS\RestBundle\Controller\Annotations\Post;
 use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations\Delete;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 
 class RendezVousRestController extends AbstractFOSRestController
@@ -24,7 +27,9 @@ class RendezVousRestController extends AbstractFOSRestController
     private $rendezVousMapper;
 
     const URI_RENDEZVOUS_COLLECTION = "/rendezVouss";
-    const URI_RENDEZVOUS_INSTANCE = "/rendezVouss/{id}";
+    const URI_RENDEZVOUS_INSTANCE = "/rendezVous/{id}";
+    const URI_RENDEZVOUSPATIENT_INSTANCE = "/rendezVouss/patient/{idPatient}";
+    const URI_RENDEZVOUSPRATICIEN_INSTANCE = "/rendezVouss/praticien/{idPraticien}";
     
     public function __construct(RendezVousService $rendezVousService, 
                                 EntityManagerInterface $entityManager,
@@ -80,6 +85,59 @@ class RendezVousRestController extends AbstractFOSRestController
             return View::create($rendezVousDto, Response::HTTP_OK, ["Content-type" => "application/json"]);
         } else {
             return View::create([], Response::HTTP_NOT_FOUND, ["Content-type" => "application/json"]);
+        }
+    }
+
+    /**
+     * @Get(RendezVousRestController::URI_RENDEZVOUSPATIENT_INSTANCE)
+     *
+     * @return void
+     */
+    public function searchRdvByIdPatient(int $idPatient) {
+        try {
+            $rendezVousDto = $this->rendezVousService->searchByIdPatient($idPatient);
+        }catch (RendezVousServiceException $e){
+            return View::create($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, ["Content-type" => "application/json"]);
+        }
+
+        if($rendezVousDto){
+            return View::create($rendezVousDto, Response::HTTP_OK, ["Content-type" => "application/json"]);
+        } else {
+            return View::create([], Response::HTTP_NOT_FOUND, ["Content-type" => "application/json"]);
+        }
+    }
+
+    /**
+     * @Get(RendezVousRestController::URI_RENDEZVOUSPRATICIEN_INSTANCE)
+     *
+     * @return void
+     */
+    public function searchRdvByIdPraticien(int $idPraticien){
+        try {
+            $rendezVousDto = $this->rendezVousService->searchByIdPraticien($idPraticien);
+        }catch (RendezVousServiceException $e){
+            return View::create($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, ["Content-type" => "application/json"]);
+        }
+
+        if($rendezVousDto){
+            return View::create($rendezVousDto, Response::HTTP_OK, ["Content-type" => "application/json"]);
+        } else {
+            return View::create([], Response::HTTP_NOT_FOUND, ["Content-type" => "application/json"]);
+        }
+    }
+
+    /**
+     * @Post(RendezVousRestController::URI_RENDEZVOUS_COLLECTION)
+     * @ParamConverter("rendezVousDTO", converter="fos_rest.request_body")
+     * @return void
+     */
+    public function create(RendezVousDTO $rendezVousDTO) {
+        try {
+            $rendezVous = new RendezVous();
+            $this->rendezVousService->persist($rendezVous, $rendezVousDTO);
+            return View::create([], Response::HTTP_CREATED, ["Content-type" => "application/json"]);
+        } catch (RendezVousServiceException $e) {
+            return View::create($e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR, ["Content-type" => "application/json"]);
         }
     }
 
--- projet-doctolib/src/Service/RendezVousService.php
+++ projet-doctolib/src/Service/RendezVousService.php
@@ -2,7 +2,10 @@
 
 namespace App\Service;
 
+use App\Entity\Patient;
+use App\Entity\Praticien;
 use App\Entity\RendezVous;
+use App\Entity\RendezVousDTO;
 use App\Mapper\RendezVousMapper;
 use App\Repository\PatientRepository;
 use App\Repository\PraticienRepository;
@@ -57,6 +60,51 @@ class RendezVousService {
         } catch(DriverException $e){
             throw new RendezVousServiceException("Un problème est technique est servenu. Veuilllez réessayer ultérieurement.", $e->getCode());
         }
+    }
+
+    public function searchByIdPatient(int $idPatient) {
+        try {
+            $rdvsPatient = $this->repository->findBy(['patient' => $idPatient]);
+            $rdvsPatientDTO = [];
+
+            foreach ($rdvsPatient as $rdv) {
+                $rdvsPatientDTO[] = $this->rendezVousMapper->transformeRendezVousEntityToRendezVousDto($rdv);
+            }
+
+            return $rdvsPatientDTO;
+
+        } catch (DriverException $e) {
+            throw new RendezVousServiceException($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function searchByIdPraticien(int $idPraticien) {
+        try {
+            $rdvsPraticien = $this->repository->findBy(['praticien' => $idPraticien]);
+            $rdvsPraticienDTO = [];
+
+            foreach ($rdvsPraticien as $rdv) {
+                $rdvsPraticienDTO[] = $this->rendezVousMapper->transformeRendezVousEntityToRendezVousDto($rdv);
+            }
+
+            return $rdvsPraticienDTO;
+
+        } catch (DriverException $e) {
+            throw new RendezVousServiceException($e->getMessage(), $e->getCode());
+        }
+    }
+
+    public function persist(RendezVous $rendezVous, RendezVousDTO $rendezVousDTO) {
+        try {
+            $patient   = $this->patientRepository->find($rendezVousDTO->getPatient());
+            $praticien = $this->praticienRepository->find($rendezVousDTO->getPraticien());
+            $rdv = $this->rendezVousMapper->transformeRendezVousDtoToRendezVousEntity($rendezVousDTO, $rendezVous,$patient, $praticien);
+            $this->entityManager->persist($rdv);
+            $this->entityManager->flush();
+        } catch (DriverException $e) {
+            throw new RendezVousServiceException($e->getMessage(), $e->getCode());
+        }
+ 
     }
 
 }
